855e6c4a9fd4563e0df0edb9d8623eaba47c9b66
athira <athiraos1997@gmail.com>
2023-11-02 01:13:17 +0530

    using props made movielist component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import React from 'react'
-import MovieCard from './MovieCard'
+import MovieList from './MovieList'
 
 function App () {
   return (
     <>
           
-           <MovieCard/>
+           <MovieList/>
     </>
   )
 }
